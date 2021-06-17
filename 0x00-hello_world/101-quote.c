43546d184d9cb7e10abb9025d7d24d566071b074
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-17 20:24:25 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-int main ()
+int main()
+/**
+*main-entry point
+*Return:Always 1(seccess)
+*/
 {
-  puts("and that piece of art is useful \" - Dora Korpar");
-  return (1);
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
 }

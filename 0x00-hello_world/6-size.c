226da21225441c45f8c3cc5328cbbe334bc9abae
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-17 22:17:46 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char c;
 float f;
 long int l;
 long long int ll;
-printf("size of a char: %zu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of an int: %zu byte(s)\n", (unsigned long)sizeof(i));
-printf("size of a long int: %zu byte(s)\n", (unsigned long)sizeof(l));
-printf("size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(ll));
-printf("size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(ll));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

b6c180e4b14e28cdf26af06a8813b8611e489765
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-17 20:29:07 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
 /**
-*main - Entry point
-*Return:Always 1(success)
+*Main - Entry point
+*Return:Always 0(success)
 */
 {
 puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");

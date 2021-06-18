10cecb3bc9aa61b0eb916cc296dce5bdb189ab48
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-18 19:06:19 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+*main-Entry point
+*
+*Return:Always0(success)
+*/
+int main(void)
+char ch='z';
+while (ch <='z')
+{
+putchar(ch);
+ch++;
+} 
+return (0);
+}
+
+
+

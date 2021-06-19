891ba1e6534c316ac4d95e10523fa5c4ff3a9323
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 08:39:25 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,16 @@
-
- #include <stdio.h>
- 
- /**
- * main - Entry point 
- * 
- * Return: 0 for success
- */
- 
+#include <stdio.h>
+/**
+*main-Entry point 
+* 
+*Return:0 for success
+*/
 int main(void)
 {
 int i;
- 
-for (i = '0'; i <= '9'; i++)
+for ( i = '0'; i <= '9'; i++)
 {
 putchar(i);
 }
 putchar(10);
 return(0);
-} 
+}

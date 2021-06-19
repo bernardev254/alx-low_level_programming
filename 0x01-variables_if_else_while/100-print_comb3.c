fbb292eaadfe305920225278517463b07439d804
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 16:49:33 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main-Entry point
+*
+*Return:0 for success
+*/
+int main(void)
+{
+int ch, c;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+{
+for (c ='0'; c <= '9'; c++)
+{
+putchar('c');
+putchar(',');
+} 
+}
+}
+putchar('\n');
+return (0);
+}

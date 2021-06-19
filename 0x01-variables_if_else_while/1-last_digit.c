7be6da6634283469f9ac1cee48fa80add2331706
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 15:51:58 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,14 +24,13 @@ if(lastdigit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n",n,mod);
 }
-else if(n>0 && n<6)
+else if((n !=0) && (n < 6))
 {
-printf("Last digit of %d is %d and is less than 6 and  not 0\n",n,mod);
+printf("Last digit of %d is %d and is less than 6 and not 0\n",n,mod);
 }
 else
 {
 printf("Last digit of %d is 0 and is 0\n",n);
 }
-
 return (0);
 }

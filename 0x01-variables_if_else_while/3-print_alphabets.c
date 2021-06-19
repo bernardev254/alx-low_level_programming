d4bf776d405f6e4dc1e3c6f1e2aae19d220e9805
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 10:19:18 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main-Entry point
+*
+*Return:0 for success
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+char c;
+for (c = 'A'; c<= 'Z'; c++) 
+{
+putchar(c);
+} 
+putchar('\n');
+return (0);
+}

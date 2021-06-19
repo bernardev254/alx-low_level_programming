e7533852b5e65455881a0d4a8df153c7bcc900b8
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 17:18:54 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,14 +6,22 @@
 */
 int main(void)
 {
-int ch, c;
+int ch, c, a, b;
 for (ch = '0'; ch <= '9'; ch++)
 {
 for (c = '0'; c <= '9'; c++)
 {
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
 putchar(ch);
 putchar(c);
+putchar(a)
+putchar(b)
 putchar(',');
+} 
+} 
 }
 }
 putchar('\n');

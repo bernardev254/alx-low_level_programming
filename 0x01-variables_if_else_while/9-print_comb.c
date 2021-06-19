43f144d0c1e136f55f46bda8f6701f5d710fc720
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-19 12:03:41 +0300

    Create 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main-Entry point
+*
+*Return:0 for success
+*/
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+putchar(',');
+putchar('');
+}
+putchar('\n');
+return (0);
+}

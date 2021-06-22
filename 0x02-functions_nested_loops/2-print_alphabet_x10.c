2f413eecfcef224246e578948cd1df9dd44bb0c1
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 17:05:55 +0300

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+/**
+*print_alphabet_x10 - prints 10 times alphabets in lower case followed by a new line
+*
+*Returns void to whoever called the function
+*/
+void print_alphabet_x10(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+}
+_putchar('\n');
+}

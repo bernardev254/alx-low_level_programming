dbd3f2d6e369fca71933fe7586273bd1232e0f4f
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 19:16:59 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-*int _islower - checks for lower case characters.
-*
-*Returns 1 if the character is lowercase and o if not.
+*_islower - checks for lower case characters.
+*@c: character to be checked
+*Return:1 if the character is lowercase and o if not.
 */
 int _islower(int c)
 {

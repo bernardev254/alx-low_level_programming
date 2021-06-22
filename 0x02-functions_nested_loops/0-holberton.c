61a1c9e54ee8f990bc7f5ad6ecea0903dc137d44
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 15:17:56 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 /**
- *main-runtime start
- *
- *return:always0
- */
+*main-runtime start
+*
+*Return:always 0 (success)
+*/
 int main()
 {
-  _putchar('H');
-  _putchar('o');
-  _putchar('l');
-  _putchar('b');
-  _putchar('e');
-  _putchar('r');
-  _putchar('t');
-  _putchar('o');
-  _putchar('n');
-  _putchar(' ');
-  return (0);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar(' ');
+return (0);
 }
+

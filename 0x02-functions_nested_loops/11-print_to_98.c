180a5ae93010afa6a9d6211bcb8f74e8a3f1631b
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 21:05:16 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,16 @@ void print_to_98(int i)
 {
 if (i <= 98)
 {
-for (i=i; i <=98; i++)
+for (i = i; i <= 98; i++)
 {
-printf("%d",i);
+printf("%d\n,", i);
 }
 }
 else
 {
-for (i=i; i >= 98; i--)
+for (i = i; i >= 98; i--)
 {
-printf("%d",i);
+printf("%d", i);
 }
 }
 }

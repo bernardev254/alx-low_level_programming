93cb0b50b10f463d8bf4a37144c16ad1e6e42cff
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 17:27:17 +0300

    Create 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*_isalpha - checks for alphabet characters
+*
+*Returns 1 if the character is an alphabet and 0 if not.
+*/
+int _isalpha(int c)
+{
+return ((c >= 'a') && (c <= 'z')) || (c >= 'A') && (c <='Z'));
+{
+  

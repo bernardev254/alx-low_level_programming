33680d00a2e60e3a239f5432da7870ec3ffd0ce9
Bkaraba <bkaraba14@gmail.com>
2021-06-22 06:16:26 -0700

    updates

---
+++ 0x02-functions_nested_loops/1-print_alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*print_alphabet:prints the alphabets in lower case followed by a new line
+*
+*returns void to whoever calls this function
+*/void print_alphabet(void)
+{
+char ch;
+for (ch ='a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar('\n');
+}
+  

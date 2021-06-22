a04568cbde2ce39cf7b18d9388feac4f8a3408cc
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 16:23:41 +0300

    Update and rename 1-print_alphabet.c to 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
 *print_alphabet:prints the alphabets in lower case followed by a new line
 *
-*returns void to whoever calls this function
+*returns void to whoever calls this function.
 */void print_alphabet(void)
 {
 char ch;

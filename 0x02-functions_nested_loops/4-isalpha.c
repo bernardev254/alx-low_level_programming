ac41f4914200689c67e4e901b2e5ff0df19480c2
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 19:05:17 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
 *_isalpha - checks for alphabet characters
-*
+*@c:charcter to be checked
 *Returns 1 if the character is an alphabet and 0 if not.
 */
 int _isalpha(int c)

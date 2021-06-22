e431405d83ca38715508a1b22d1dc75df54af749
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 17:15:27 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+/**
+*int _islower - checks for lower case characters.
+*
+*Returns 1 if the character is lowercase and o if not.
+*/
+int _islower(int c)
+{
+if (c >= UC('a') && c <= UC('z'))
+{
+return (1);
+}
+return 0;
+}

afd33d38556c2927a714f85bbfd17c43bd633d04
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-22 18:08:41 +0300

    Create 8-24_hour.c

---
+++ 0x02-functions_nested_loops/8-24_hour.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*jack_bauer -  that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+*
+*Returns - void to whoever called the function.
+*/
+void jack_bauer(void)
+{
+char a,b,c,d; 
+{
+for (a = '0'; a <= '2': a++)
+{
+for (b = '0'; b <= '2': b++)
+{
+for (c = '0'; c <= '2': c++)
+{
+for (d = '0'; d <= '2': d++)
+{
+_putchar(a);
+_putchar(b);
+_putchar(c);
+_putchar(d);
+}
+}
+}
+}
+}

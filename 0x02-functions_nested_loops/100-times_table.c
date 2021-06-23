790b650bb36943908cfca024eea131d91c99df9b
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 18:33:22 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,22 +1,20 @@
 #include "holberton.h"
 /**
 *times_table - prints times table for n.
-*
 *Return: void.
 */
 void print_times_table(int n)
 {
-int i = 0, j = 0, product = (i * j), last = ((i * j) % 10), first = ((i * j) / 10);
+int i , j;
 if ((n <= 15) && (n >= 0))
+product = (i * j), last = ((i * j) % 10), first = ((i * j) / 10);
 {
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 if (j == 0)
-{
 _putchar('0');
-}
 else if (product < 10)
 {
 _putchar(',');

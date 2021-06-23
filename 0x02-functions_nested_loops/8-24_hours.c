64eef7421ebb36d948af00bafa1fcc0b471bbbf3
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 13:09:18 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ for (a = 0; a < 24; a++)
 {
 for (b = 0; b <= 59; b++)
 {
-_putchar((a / 10));
-_putchar((a % 10));
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
 _putchar(':');
-_putchar((b / 10));
-_putchar((b % 10));
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
 _putchar('\n');
 }
 }

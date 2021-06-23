504d523de5f30f6b08d74517996ca2d00ac1c823
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 13:06:43 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 int a, b;
 {
-for (a = '0'; a < '24'; a++)
+for (a = 0; a < 24; a++)
 {
-for (b = '0'; b <= '59'; b++)
+for (b = 0; b <= 59; b++)
 {
 _putchar((a / 10));
 _putchar((a % 10));

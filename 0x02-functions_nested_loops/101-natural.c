c5debafce6bda078f5c94efb92166c9bd1c418e1
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 19:40:26 +0300

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*add - adds two integers
+*@a:integer for adding
+*@b:integer for adding
+*Return:the sum of two integers.
+*/
+int main(int a)
+{
+for (a > 0)
+{
+if ((a % 3 ==0) || (a % 5 == 0))
+a++;
+while (a < 1024)
+{
+printf(
+return ((a + b));
+}

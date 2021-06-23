6255ba78db8a05fbf88d004a51d11959254be9eb
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 18:28:49 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
 */
 void print_times_table(int n)
 {
-int i, j, product = (i * j), last = ((i * j) % 10), first = ((i * j) / 10);
+int i = 0, j = 0, product = (i * j), last = ((i * j) % 10), first = ((i * j) / 10);
 if ((n <= 15) && (n >= 0))
 {
 for (i = 0; i <= n; i++)

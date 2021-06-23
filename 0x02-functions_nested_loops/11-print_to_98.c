988bfdab5fb9371ccb53ee972b654a9c1480804b
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 14:23:23 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,22 +1,24 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-*print_to_98 -  prints all natural numbers from n to 98, followed by a new line.
+*print_to_98 -  prints all natural numbers from n to 98.
 *@i:entered integer
 *Return : void.
 */
 void print_to_98(int i)
 {
-while (i !=98)
+while (i != 98)
 {
 if (i < 98)
 {
-printf("%d, ",i);i++;
+printf("%d, ", i);
+i++ ;
 }
 else if (i > 98)
 {
-printf("%d, ",i);i--;
+printf("%d, ", i);
+i-- ;
 }
 }
-printf("%d\n",i);
+printf("%d\n", i);
 }

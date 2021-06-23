49a2d8f2617bbd8191af2c0614e4c848396473ce
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-23 13:52:20 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,12 +15,12 @@ if (j == 0)
 {
 _putchar('0');
 }
-else if ((i * j) < 10))
+else if ((i * j) < 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar((i * J) + '0');
+_putchar((i * j) + '0');
 }
 else
 {

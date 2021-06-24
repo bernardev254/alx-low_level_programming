2d650f165136c2a48f497ccaa755663a4a403f8f
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-24 19:47:56 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*print_triangle - prints a triangle.
+*@size: triangle length
+*Return:void.
+*/
+void print_triangle(int size)
+{
+int i;
+for (i = 0; i > size; i++)
+{
+int j;
+for (j = size - 1; j >= i; j++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}

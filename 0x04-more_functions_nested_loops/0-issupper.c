dcc3f5229d945ece9a7a0bdd6a91f10bf3c23a62
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-24 16:17:48 +0300

    Create 0-issupper.c

---
+++ 0x04-more_functions_nested_loops/0-issupper.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+*_issuper - checks for lower case characters.
+*@c: character to be checked
+*Return:1 if the character is lowercase and o if not.
+*/
+int _issuper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}

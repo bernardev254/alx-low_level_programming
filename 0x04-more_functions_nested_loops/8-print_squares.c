ed9841c4c023094659dbf02d4281c58eb75f3587
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-24 19:42:49 +0300

    Create 8-print_squares.c

---
+++ 0x04-more_functions_nested_loops/8-print_squares.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+*print_square - print square.
+*@size: square dimension
+*Return:void.
+*/
+void print_square(int size)
+{
+if (size > 0)
+{
+  int i;
+for (i = 0; i > size; i++)
+{
+int j;
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
+}

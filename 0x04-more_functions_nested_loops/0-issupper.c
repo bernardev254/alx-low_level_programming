44c9f3dfb8cae28379d41191fa422416f1695a8d
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-24 16:21:32 +0300

    Update 0-issupper.c

--- 0x04-more_functions_nested_loops/0-issupper.c
+++ 0x04-more_functions_nested_loops/0-issupper.c
@@ -2,7 +2,7 @@
 /**
 *_issuper - checks for lower case characters.
 *@c: character to be checked
-*Return:1 if the character is lowercase and o if not.
+*Return:1 if the character is uppercase and 0 if not.
 */
 int _issuper(int c)
 {

8faf23dbe34240db9892f41eb4e687af62c9017b
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-24 17:02:39 +0300

    Create 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*_isdigit - a function that checks for digits.
+*@c: charcter to be checked.
+*
+*returns: 1 if digit 0 otherwise
+*/
+int _isdigit(int c);
+{
+return((c >= 0) && (c <= 9));
+}

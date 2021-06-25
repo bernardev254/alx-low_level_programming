c4e19300d5686e04af42cdbaf4cdc31c51170dda
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-25 18:29:31 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,13 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i = 100)
+if (i == 100)
 {
 printf("Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
 }
 else if (i % 3 == 0)
 {
@@ -20,10 +24,6 @@ printf("Fizz ");
 else if (i % 5 == 0)
 {
 printf("Buzz ");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
 }
 else
 {

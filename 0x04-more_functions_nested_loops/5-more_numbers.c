ef058cdce63fa7bc175e735c80cbd507f403f3f7
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-25 11:56:18 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 int i, j;
 for (i = '0'; i < '10'; i++)
 {
-for (j = '0'; j < '14'; j++)
+for (j = '0'; j < '15'; j++)
 {
 if (j >= '10')
 {
-_putchar(j + '0');
+_putchar((j / 10) + '0');
 }
-_putchar(j);
+_putchar(j % 10) + '0');
 }
 _putchar('\n');
 }

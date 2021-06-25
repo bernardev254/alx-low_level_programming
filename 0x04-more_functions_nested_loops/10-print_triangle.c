80d931565ca853b3555b8b466f482ed7cf70b631
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-25 17:25:51 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,6 +6,7 @@
 */
 void print_triangle(int size)
 {
+if (size > 0)
 int i, j, k;
 for (i = 1; i <= size; i++)
 {
@@ -16,6 +17,8 @@ _putchar(' ');
 for (k = 1; k <= i; k++)
 {
 _putchar('#');
+}
+_putchar('\n');
 }
 _putchar('\n');
 }

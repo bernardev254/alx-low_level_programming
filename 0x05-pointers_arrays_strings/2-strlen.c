c38ab6eab4ccc6cb86a11dfca9b0f8d7355078a4
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-28 17:05:18 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,11 +10,12 @@
 int _strlen(char *s)
 {
 char *ptr;
+  long int *longword, longword;
 int length;
-for (ptr = s; ptr != 0; ptr++)
+for (ptr = s; ((long int) ptr & (sizeof (longword) - 1)) != 0; ++ptr)
 {
 if (*ptr == '\0')
 length = (ptr - s);
 }
-  return (length);
+return (length);
 }

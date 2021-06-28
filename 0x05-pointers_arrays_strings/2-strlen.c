ce4b909c575be37127a056e28278207f9d225c0f
Bkaraba <bkaraba14@gmail.com>
2021-06-28 04:34:06 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@c: pointer
+ *
+ *Return:length of a string.
+ */
+
+int _strlen(char *c)
+{
+for (s = str; *s; s++)
+{
+return (s - str);
+}
+}

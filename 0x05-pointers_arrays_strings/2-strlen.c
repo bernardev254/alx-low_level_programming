9603a1ae1defbf10c36740bbbb7e71199ddfd44e
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-29 12:42:39 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -11,9 +11,16 @@ int _strlen(char *s)
 {
 char *ptr;
 int length;
+if (s = "")
+{
+length = 0;
+}
+else
+{
 for (ptr = s; *ptr; ++ptr)
 {
 length = ((ptr - s) + 1);
+}
 }
 return (length);
 _putchar('\n');

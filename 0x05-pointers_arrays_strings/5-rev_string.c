20f6e68e2dad153032d13cd9e44c47333087f98f
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-29 15:12:52 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+*rev_string - prints a string  in reverse followed by a new line.
+*@s:pointer
+*
+*return:void
+*/
+
+void rev_string(char *s)
+{
+int len, i, n;
+char c;
+len = 0;
+while (*s != 0)
+{
+len++;
+s++;
+}
+s = s - len;
+n = len - 1;
+for (i = 0; i < len / 2; i++)
+{
+c = *(s + 1);
+*(s + 1) = *(s + n);
+*(s + n) = c;
+}
+}

64bd1c9fa9f185fe8972a985dbb0a48f57ef9d5e
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-06-29 15:31:21 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *puts2 - returns the length of a string
+ *@s: pointer
+ *
+ *Return:alternate characters of a string.
+ */
+
+void puts2(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+s++;
+if ((length % 2) = 0)
+_putchar(*s);
+}
+length++;
+}

9c55baf7de7296011d91aaca9332ccbb009d8d39
Bkaraba <bkaraba14@gmail.com>
2021-06-30 08:13:37 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *reverse_array - reverses the content of the array.
+ *@a:pointer to array'
+ *@n:no of element of the array
+ *
+ *Return:void
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+for (i = (n - 1); i >= 0; i--)
+{
+  _putchar(*(a + i));
+}
+_putchar('\n');
+}

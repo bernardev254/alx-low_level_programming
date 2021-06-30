319c19a02ab881d47d4188634dc556e6853e9cd7
Bkaraba <bkaraba14@gmail.com>
2021-06-30 06:14:26 -0700

    updates

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *_strncpy - ccopies a string.
+ *@dest: pointer param.
+ *@src: pointer param.
+ *@n:int n.
+ *
+ *Return:pointer to copied string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+char *start = dest;
+if ((dest == NULL) && (src == NULL))
+return (NULL);
+while (*src && n--)
+*dest = *src;
+dest++;
+src++;
+*dest = '\0';
+return (start);
+}

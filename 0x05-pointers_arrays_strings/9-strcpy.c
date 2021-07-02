a011f6cf89aae6a2dc61d83b4b4c5546cc9962ad
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 17:34:40 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,11 +14,7 @@ length = 0;
 for (i = 0; length != '\0'; i++)
 length++;
 for (i = 0; src[i] != '\0'; i++)
-*dest(length + i) = *src +i;
-*dest(length + i) = '\0';
+dest(length + i) = src + i;
+dest(length + i) = '\0';
 return (dest);
-}
-print_array(array, 0);
-print_array(array, -1);
-return (0);
 }

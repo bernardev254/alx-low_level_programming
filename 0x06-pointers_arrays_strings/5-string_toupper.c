ce36cf58a547fccf347dcd9ea7c823cba829529a
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 13:36:48 +0300

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *string_toupper - converts a string to uppercase.
+ *@str: pointer to string param.
+ *
+ *Return:pointer to uppercased string.
+ */
+
+char *srting_toupper(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] = str[i] - 32;
+}
+return (str);
+}

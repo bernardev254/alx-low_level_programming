af97cffaf185275c284fe70c15b5928c94ed3785
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 15:05:48 +0300

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*leet - encodes a string to 1337.
+*@str:string to be encoded
+*
+*Return:pointer to the encoded string
+*/
+
+char *leet(char *str)
+{
+int i, j;
+char change[] = "AaEeOoTtLl";
+char to[] = "4433007711"
+for (i = 0; str[i] = '\0'; i++)
+{
+for (j = 0; change[j] != '\0' && to[j] != '\0'; j++)
+{
+if (str[i] == change[j])
+{
+str[i] = to[j];
+}
+}
+}
+return (str);
+}

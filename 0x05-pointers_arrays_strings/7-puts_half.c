091db3dd55e61456377e6a5939574c7718b29088
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 16:15:36 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,14 @@ void puts_half(char *str)
 {
 int length, n, i;
 length = strlen(str);
+if (length % 2 == 0)
+{
 n = length / 2;
-  
+}
+else
+{
+n = (length + 1) / 2;  
+}
 for (i = n; i < length; i++)
 {
 _putchar(*(str + i));

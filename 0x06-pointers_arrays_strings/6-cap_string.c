881c406596159a924f6e683bc599601fcf7393e1
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 14:05:18 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+*cap_string - capitalizes a string.
+*@str: the string to be capitalized.
+*
+*Return:pointer to the capitalized string
+*/
+
+char *cap_string(char *str)
+{
+int i, j;
+char *btn = ",\n:.!?\"(){}\t[];";
+for (i = 0; str[i] != '\0'; i++)
+for (j = 0; btn[j] != '\0'; j++)
+{
+if (i == 0)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+else if (*(str + i) == btn[j] || *(str + i) == '\n')
+{
+i++;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}

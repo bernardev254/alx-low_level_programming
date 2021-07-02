72ddef52fc62ccb13fdbb26db0e1be6411183b44
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 11:16:54 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,14 +12,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-for (i = 0; i < n && *(src + i) != '\0'; i++)
+int i, len = 0;
+for (i = 0; i < dest[i] != '\0'; i++)
+len++;
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-*(dest + i) = *(src + i);
+dest[len + i] = src[i];
 }
-for (; i < n; i++)
-{
-*(dest + i) = '\0';
-}
+dest[len + i] = '\0';
 return (dest);
 }

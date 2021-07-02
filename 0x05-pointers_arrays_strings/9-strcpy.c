dcd217def6cf451a650926656934fdb243ad8379
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 17:31:27 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * strcpy - copies a string to another string.
+ *_strcpy - copies a string to another string.
  *@dest:string to append to
  *@src:string to append
  *
  * Return:pointer to the copied string.
  */
-char strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 int i,length;
 length = 0;

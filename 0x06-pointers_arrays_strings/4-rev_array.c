fefca19f7075fc1310161dbbaf5eb9b4413bb8f9
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-02 13:27:46 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 {
 int i, j;
 int tmp;
-for (i = 0; j = n - 1; i < j; i++, j--)
+for (i = 0, j = n - 1; i < j; i++, j--)
 {
 tmp = *(a + i);
 *(a + i) = *(a + j);

8c8047168a87d94753ded5d8916d07a4de34d1f6
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-05 15:32:59 +0300

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_strchr - locates a character in a string
+ *@s:pointer to the string 
+ *@c:charcter to be located
+ *
+ *Return:pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+char *ptr = s;
+while (*ptr != '\0')
+{
+if (*ptr == c)
+{
+*ptr = c}
+}
+return (s);
+}

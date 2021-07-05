09dad59fe8dd4a8eeb2731b344957b6f45c8e171
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-05 16:48:32 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- *_strspn - returns character after accept
+ *_strspn - gets the len of a prefix substring
  *@s:pointer to mem location
- *@accept:characters 
+ *@accept:pointer to string
  *
- *Return:string
+ *Return:no of bytes from the source substring
  */
 
 unsigned int _strspn(char *s, char *accept)

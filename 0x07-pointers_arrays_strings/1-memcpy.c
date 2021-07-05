c2fb006b36bea84482ff4b6c0a7c6a5472613ead
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-05 15:13:57 +0300

    Create 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_memcpy - copies memory area
+ *@dest:pointer to mem location
+ *@src:pointer to source 
+ *@n:number of to copy
+ *
+ *Return:pointer
+ */
+
+char *_memset(char *dest, char *src, unsigned int n)
+{
+char *ptr = dest;
+char *sprt = src;
+while (n-- > 0)
+*dptr++ = *sptr++;
+return (dest);
+}

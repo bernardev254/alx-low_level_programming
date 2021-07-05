9bf121267bce5061dd93c2a0291114b97cea4942
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-05 15:05:49 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,8 +10,8 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  char *ptr = s;
-  while (n-- > 0)
-    *ptr++ = b;
-  return (s);
+char *ptr = s;
+while (n-- > 0)
+*ptr++ = b;
+return (s);
 }

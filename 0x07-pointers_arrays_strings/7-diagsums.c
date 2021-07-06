b51f8ec2fdb1d646f2f1c2659dd5343fa8755dfd
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-06 12:58:17 +0300

    Create 7-diagsums.c

---
+++ 0x07-pointers_arrays_strings/7-diagsums.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_diagsums- prints the sum of two diagonals
+ *@a:pointer
+ *@size:size
+ *
+ *Return:void
+ */
+
+void *print_diagsums(int *a, int *size)
+{
+int i, j, sum = 0;
+for (i = 0; i < size;  i++)
+{
+sum = sum + *(a + i)
+}
+printf("%d", sum);
+sum = 0;
+for (j = 0; j < size; j++)
+{
+sum = sum + *(a + size * (j + 1) - (j + 1);
+}
+printf("%d\n",sum);
+}

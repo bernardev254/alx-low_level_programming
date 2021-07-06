9ca57148beff1a599ce411ba4e1a46948a553b47
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-06 14:02:42 +0300

    Update 7-diagsums.c

--- 0x07-pointers_arrays_strings/7-diagsums.c
+++ 0x07-pointers_arrays_strings/7-diagsums.c
@@ -13,13 +13,13 @@ void *print_diagsums(int *a, int *size)
 int i, j, sum = 0;
 for (i = 0; i < size;  i++)
 {
-sum = sum + *(a + i)
+sum = sum + *(a + i *size + i);
 }
 printf("%d", sum);
 sum = 0;
 for (j = 0; j < size; j++)
 {
-sum = sum + *(a + size * (j + 1) - (j + 1);
+sum = sum + *(a + size *(j + 1) - (j + 1);
 }
-printf("%d\n",sum);
+printf("%d\n", sum);
 }

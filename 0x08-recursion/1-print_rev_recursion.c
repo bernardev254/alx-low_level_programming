007f86e44a0cd000f8f53017f13d794ba1c313b6
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 12:07:16 +0300

    Create 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+
+/**
+ *print_rev_recursion -prints a string in reverse.
+ *@s:pointer to string.
+ *
+ *Return:void
+ */
+
+void print_rev_recursion(char *s)
+{
+char *a = s;
+*a = '\0';
+if (*a != *s)
+{
+_putchar(*s);
+a--;
+print_rev_recursion(s);
+}
+else
+{
+_putchar('\n');
+}
+}

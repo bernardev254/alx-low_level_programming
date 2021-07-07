edadcd048e9f1016c302f94f6dc5791324553773
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 12:31:20 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,13 @@
 
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
-_putchar('\n');
+if (*s != '\0')
 {
 s++;
 _print_rev_recursion(s);
 _putchar(*s);
-_putchar('\n');
 }
+else
+{
+_putchar('\n');
 }

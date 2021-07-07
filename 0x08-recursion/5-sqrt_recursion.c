a60b0b8c3c6c7ae992b6637f005445aea8bbbd88
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 22:10:44 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,7 +17,7 @@ return (func(root + 1, square));
 }
 
 /**
- *_sqtr_recursion -prints natural sqrt.
+ *_sqrt_recursion -prints natural sqrt.
  *@n:integer to compute its sqtr.
  *
  *Return:sqrt of n.

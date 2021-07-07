a657bf46acef9fc887c4edb44db412f3d1b3cea0
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 15:18:03 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,10 +10,10 @@
 
 int _pow_recursion(int x, int y)
 {
-if (y == 0) /* base condition */
+if (y <= 0) /* base condition */
 return (1);
 else if (y % 2 != 0)
-return (x * _pow_recursion(x, y/2) * _pow_recursion(x, y/2));
+return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 else
-return (_pow_recursion(x, y/2) * _pow_recursion(x, y/2));
+return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }

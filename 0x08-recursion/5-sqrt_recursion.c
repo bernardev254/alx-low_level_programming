7293188208b7bbac92e5ecc9fc3039405c5e1445
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 17:51:12 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,25 @@
  */
 
 int _sqtr_recursion(int n)
-
+{
+/**_pow_recursion - returns power.
+*@x:base
+*@y:exponent
+*
+*Return:int
+*/
+int _pow_recursion(int x, int y)
+{
+if (y <= 0) /* base condition */
+if (y < 0)
+return (-1);
+return (1);
+}
+else if (y % 2 != 0)
+return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+else
+return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+}
 {
 return (_pow(2, 0.5 * log2(n));
 }

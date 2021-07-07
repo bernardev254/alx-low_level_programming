55845c1f4c636416fa9b9a3bb5da3da76a0be288
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 15:16:09 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- *pow_recursion -prints x power y .
+ *_pow_recursion -prints x power y .
  *@x:integer to compute its factorial.
  *@y:integer
  *
  *Return:x power y.
  */
 
-int pow_recursion(int x, int y)
+int _pow_recursion(int x, int y)
 {
 if (y == 0) /* base condition */
 return (1);
 else if (y % 2 != 0)
-return (x * pow_recursion(x, y/2) * pow_recursion(x, y/2));
+return (x * _pow_recursion(x, y/2) * _pow_recursion(x, y/2));
 else
-return (pow_recursion(x, y/2) * pow_recursion(x, y/2));
+return (_pow_recursion(x, y/2) * _pow_recursion(x, y/2));
 }

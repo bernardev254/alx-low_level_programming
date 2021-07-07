0be5c84d06e93a457dc11dad541ff545c2ae0323
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 10:07:22 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,10 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-  if (*s++ != '\0')
-    {
-      _putchar(*s);
-      _puts_recursion(char *s++);
-    }
-  _putchar('\n');
+if (*s++ != '\0')
+{
+_putchar(*s);
+_puts_recursion(char *s++);
+}
+_putchar('\n');
 }

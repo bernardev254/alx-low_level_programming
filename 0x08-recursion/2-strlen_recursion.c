c939ddd826645c18557ebc058c4872309e859c17
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 13:26:07 +0300

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strlen_recursion -prints a string in reverse.
+ *@s:pointer to string.
+ *
+ *Return:void
+ */
+
+int _strlen_recursion(char *s)
+{
+n = 0;
+if (*s != '\0')
+{
+s++;
+n++;
+return (n);
+_strlen_recursion(*s);
+}
+return (n);
+return (n);
+}

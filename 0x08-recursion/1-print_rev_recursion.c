444d4e8e53ab5e9020fafca71140addf15733cb6
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 12:12:26 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- *print_rev_recursion -prints a string in reverse.
+ *_print_rev_recursion -prints a string in reverse.
  *@s:pointer to string.
  *
  *Return:void
  */
 
-void print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 char *a = s;
 *a = '\0';
@@ -14,7 +14,7 @@ if (*a != *s)
 {
 _putchar(*s);
 a--;
-print_rev_recursion(s);
+_print_rev_recursion(s);
 }
 else
 {

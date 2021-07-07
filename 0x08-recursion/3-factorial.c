ed826dce03f6da90f96e45cd7a532a18a96aef8b
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-07 13:41:24 +0300

    Create 3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *factorial -prints a string.
+ *@n:integer to compute its factorial.
+ *
+ *Return:factorial if n.
+ */
+
+int factorial(int n)
+{
+if (n <= 0)
+return (1);
+{
+return (n * factorial(n - 1));
+}
+}

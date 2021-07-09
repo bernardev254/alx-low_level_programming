8c9163566f5dd2adbd64fcc08459eadc3b5a50bf
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 12:00:37 +0300

    Create 3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *_strcmp - compares two strings.
+ *@s1: pointer param pointing to first string.
+ *@s2:pointer param pointing to second string.
+ *
+ *Return: integer.
+ */
+int _strcmp(char *s1, char *s2)
+{
+for ( ; *s1 == *s2; s1++, s2++)
+if (*s1 == '\0')
+return (0);
+return (*(s1) - *(s2));
+}

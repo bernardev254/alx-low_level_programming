c917f111f20eebfa867885a2900d008d7db0141d
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 12:02:06 +0300

    Create 0-memset.c

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_memset - fills memory with a constant byte
+ *@s:pointer to mem location
+ *@b:character to fill
+ *@n:number of n to fill
+ *
+ *Return:pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *ptr = s;
+while (n-- > 0)
+*ptr++ = b;
+return (s);
+}

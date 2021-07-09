5c30d51661c5e4a40c8c2c82cfb49f5f54ede842
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 11:59:31 +0300

    Create 2-strncpy.c

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *_strncpy - copies a string.
+ *@dest: pointer param.
+ *@src: pointer param.
+ *@n:int n.
+ *
+ *Return:pointer to copied string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && *(src + i) != '\0'; i++)
+{
+*(dest + i) = *(src + i);
+}
+for (; i < n; i++)
+{
+*(dest + i) = '\0';
+}
+return (dest);
+}

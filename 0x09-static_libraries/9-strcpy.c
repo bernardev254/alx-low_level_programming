7ba988cb6dcaece42c9e2a9df2eb2acea24868f5
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 11:54:00 +0300

    Create 9-strcpy.c

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_strcpy - copies a string to another string.
+ *@dest:string to append to
+ *@src:string to append
+ *
+ * Return:pointer to the copied string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+*(dest + i) = *(src + i);
+*(dest + i) = '\0';
+return (dest);
+}

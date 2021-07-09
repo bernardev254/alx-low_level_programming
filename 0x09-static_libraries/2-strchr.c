9bfa8b3eb761a81dab63c674e5a83dfc7a7ad6f0
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 12:04:54 +0300

    Create 2-strchr.c

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strchr - locates a character in a string
+ *@s:pointer to the string
+ *@c:charcter to be located
+ *
+ *Return:pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+int a;
+for (a = 0; s[a] >= '\0'; a++)
+{
+if (s[a] == c)
+{
+return (s + a);
+}
+}
+return ('\0');
+}

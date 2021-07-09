5abe0895e8478810c48036e9fadee3b6fae30436
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 11:44:48 +0300

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+*_isalpha - checks for alphabet characters
+*@c:charcter to be checked
+*Return: 1 if the character is an alphabet and 0 if not.
+*/
+int _isalpha(int c)
+{
+return ((((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z'))));
+}

41348a5f94dec7094674504ea443b084ee1c9c12
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 12:03:41 +0300

    Create 1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_memcpy - copies memory area
+ *@dest:pointer to mem location
+ *@src:pointer to source
+ *@n:number of to copy
+ *
+ *Return:pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *dptr = dest;
+char *sptr = src;
+while (n-- > 0)
+*dptr++ = *sptr++;
+return (dest);
+}

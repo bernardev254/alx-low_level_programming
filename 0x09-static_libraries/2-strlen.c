c29a13873aed03ff2df375959c6f2e684a0cc3ca
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 11:51:43 +0300

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: pointer
+ *
+ *Return:length of a string.
+ */
+
+int _strlen(char *s)
+{
+int length = 0;
+while (*s != '\0')
+{
+++length;
+s++;
+}
+return (length);
+}

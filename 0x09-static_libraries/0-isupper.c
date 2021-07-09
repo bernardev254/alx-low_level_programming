3464a4450694dc3b7fa0a77e74dae703af6dbcc6
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 11:46:06 +0300

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*_isupper - checks for lower case characters.
+*@c: character to be checked
+*Return:1 if the character is uppercase and 0 if not.
+*/
+int _isupper(int c)
+{
+return ((c >= 'A') && (c <= 'Z'));
+return (0);
+}

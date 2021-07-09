506beebdb98f4c667856320a1dda39d46646a360
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 12:06:34 +0300

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *_strspn - gets the len of a prefix substring
+ *@s:pointer to mem location
+ *@accept:pointer to string
+ *
+ *Return:no of bytes from the source substring
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int n = 0;
+int b;
+if (!(*s) || !(*accept))
+{
+return (n);
+}
+while (*s)
+{
+for (b = 0; accept[a] != '\0'; b++)
+{
+if (accept[b] == *s)
+{
+n++;
+break;
+}
+else if (accept[b + 1] == '\0')
+return (n);
+}
+s++;
+}
+return (n);
+}

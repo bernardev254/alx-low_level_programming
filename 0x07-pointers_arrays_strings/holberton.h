b0da06454c7fb086fba6eb4af9d78efd0712beb4
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-09 16:49:21 +0300

    Update holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -5,5 +5,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 char *_strchr(char *s, char c);
-void print_chessboard(char *(a)[8]);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);

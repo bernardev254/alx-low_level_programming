cd6b78678451aa3ba4568a16c8ba351fab8b09c6
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 22:59:50 +0300

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,9 +19,9 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] >= '0' && argv[i][j] <= '9')
+if (isdigit(argv[i][j]))
 {
-sum += atoi(argv[i][j]);
+sum += atoi(argv[i]);
 printf("%d\n", sum);
 return (1);
 }

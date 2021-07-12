580dfbc44b5f7efde3cc3f265bbc1924e5098177
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 10:04:47 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: arguments count
+ *@argv:arguments vector 
+ *
+ * Return: On success 0.
+ */
+void main(int argc, char **argv)
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+_putchar(argv[i]);
+_putchar('\n');
+}
+}

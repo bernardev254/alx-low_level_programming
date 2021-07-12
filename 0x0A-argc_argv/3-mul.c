e986dfa3dfc4706069a0a6a9089daf6ebf15eeb4
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 10:19:40 +0300

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: arguments count
+ *@argv:arguments vector 
+ *
+ * Return: On success 0
+ */
+int main(int argc, char **argv)
+{
+char *s = "Error";
+int result = 0;
+int a = argv[1];
+int b = argv[2];
+if (argc == 3)
+{
+result = a * b;
+_putchar('result');
+}
+else
+{
+_putchar(s);
+return (1);
+}
+_putchar('\n');
+return (0);
+}

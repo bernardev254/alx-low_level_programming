a981c1c633a584ee247a1c4127578445847dea81
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 09:55:41 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - prints the number of arguments passed to it.
+ * @argc: arguments count
+ *@argv:arguments vector 
+ *
+ * Return: On success 0.
+ */
+int main(int argc, char **argv)
+{
+int a = argc;
+_putchar('a');
+_putchar('\n');
+return (0);
+}

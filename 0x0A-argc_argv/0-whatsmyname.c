89adb07549e2d558a000c21f9eb14ad2d478533c
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 13:04:41 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,11 @@
  * @argc: arguments count
  *@argv:arguments vector
  *
- * Return: void.
+ * Return: 0 on success.
  */
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 if (argc > 0)
 printf("%s\n", argv[0]);
+return (0);
 }

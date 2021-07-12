f0c131e9b954154044225d1dd7da2eea27ead01c
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-12 08:33:16 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+
+/**
+ * main - writes the character c to stdout
+ * @argc: arguments count
+ *@argv:arguments vector 
+ *
+ * Return: On success 1.
+ */
+int main(int argc, char **argv)
+{
+char *s = **argv;
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+_putchar('\n');
+return (1);
+}

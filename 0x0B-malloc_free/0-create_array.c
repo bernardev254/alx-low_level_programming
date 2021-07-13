e16cae2f4ee45286b3f3ea56ecc420341e5c5936
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-13 15:39:56 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
 *create_array - creates an array.
 *@size: int.
@@ -15,7 +16,7 @@ char *create_array(unsigned int size, char c)
   {
     ar = malloc(size * sizeof(char));
     if (ar == NULL)
-      fprintf("strderr,failled to allocate memory\n")
+      fprintf(strderr,"failled to allocate memory\n")
     for (i = 0; i < size; i++)
       ar[i] = c;
   }

0709906918c2ed4b5e9a247effce9e5f994e0892
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-13 16:16:43 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,10 @@ if (size == 0)
 return (NULL);
 ar = malloc(size * sizeof(char));
 if (ar == NULL)
+{
 fprintf(stderr, "failled to allocate memory\n");
+return (1);
+}
 for (i = 0; i < size; i++)
 ar[i] = c;
 }

57e3e7c8fdc2d9c30cc4d42cee5720c5878d1ad8
Bkaraba <bkaraba14@gmail.com>
2021-07-12 23:36:58 -0700

    updates

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -13,7 +13,7 @@ int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-char _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);

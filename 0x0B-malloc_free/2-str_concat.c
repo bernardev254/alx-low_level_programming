ac01b4a4b4398d38c0c19c40c8501960214053bd
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-14 14:13:53 +0300

    Create 2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+*_strdup - duplicates a string.
+*@str: string.
+*
+*Return: pointer to string duplicate
+*/
+
+char *_strdup(char *str)
+{
+char *ar;
+int len;
+{
+if (str == NULL)
+return (NULL);
+len = strlen(str) + 1;
+ar = (char *)malloc(sizeof(char) * len);
+if (ar == NULL)
+{
+fprintf(stderr, "failled to allocate memory\n");
+return (ar);
+}
+}
+return (memcpy(ar, str, len));
+}

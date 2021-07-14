9ff880de32e5fdd1cad0730c8b3e471f70d2d20c
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-14 17:50:02 +0300

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+*alloc_grid - returns a pointer to 2d array
+*@width:array width
+*@height: array height
+*
+*Return: pointer to array
+*/
+
+int **alloc_grid(int width, int height)
+{
+int **arrr = NULL, *arr = NULL;
+int i, j;
+if(width == NULL || height == NULL)
+return (NULL);
+arrr = malloc((sizeof(int*) * height)+(sizeof(int) * height * width));
+if (arrr != NULL)
+for (i = 0;i < height; i++)
+{
+arrr[i] = arr + (i * width);
+for (j = 0; j < width; j++)
+arrr[i][j] = 0;
+}
+else
+return (free(arrr), NULL);
+return (arrr);
+}

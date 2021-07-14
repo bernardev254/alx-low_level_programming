f8c4caa6c958fc1422f96236582e786809464507
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-14 15:40:34 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,22 +14,28 @@ char *str_concat(char *s1, char *s2)
 {
 char *ar;
 int len, len2;
-/**
 int i = 0;
 int j = 0;
-*/
 len = strlen(s1);
 len2 = strlen(s2);
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-ar = malloc(sizeof(*s1) * (len2 + len)+sizeof(char));
+ar = malloc(sizeof(*s1) * (len2 + len));
 if (ar == NULL)
 {
 fprintf(stderr, "failled to allocate memory\n");
 return (NULL);
 }
-ar = strcat(s1, s2);
+while(i < len)
+{
+ar[i] = as1[i];
+i++;
+}
+while(j <= len1)
+ar[j] = s2[j];
+j++;
+}
 return (ar);
 }

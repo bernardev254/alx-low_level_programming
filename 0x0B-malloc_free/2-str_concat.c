051d6099c39ad41fe49e78e80c243c91b5c10f3d
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-14 15:20:38 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-ar = malloc(sizeof(*s1) * (len2 + len));
+ar = malloc(sizeof(*s1) * (len2 + len)+(sizeof(char));
 if (ar == NULL)
 {
 fprintf(stderr, "failled to allocate memory\n");

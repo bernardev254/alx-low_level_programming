08b370f05682bcad5660cf3e2af99042d3311944
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-14 13:04:38 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *ar;
 {
 if (str == NULL)
 return (NULL);
-ar = malloc(sizeof(char *));
+ar = char *malloc(sizeof(char) * strlen(str) + 1);
 if (ar == NULL)
 {
 fprintf(stderr, "failled to allocate memory\n");

e2c4f6feba3d9ce479e37ce66c83cbde1b1d23c2
Bkaraba <bkaraba14@gmail.com>
2021-07-20 09:57:16 -0700

    patch

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print-dog(struct dog *d);
+void print_dog(struct dog *d);
 #endif
 

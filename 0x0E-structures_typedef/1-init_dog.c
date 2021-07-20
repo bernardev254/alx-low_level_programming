9f7202bd5fec4dd211e41fe977f925ac3dbdf02a
Bkaraba <bkaraba14@gmail.com>
2021-07-20 02:34:27 -0700

    patch

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+/**
+ * init_dog - init dog stuct.
+ * @d: struct pointer variable.
+ *@name: name pointer variable.
+ *@age:age variable.
+ *@owner:owner variable.
+ *
+ *Return : void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+};
+

37e08b4d56b9e4fc0d3c5d53ad582499c719b641
Bkaraba <bkaraba14@gmail.com>
2021-07-22 10:01:39 -0700

    patch

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * op_add - returns addition of two ints
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns sub of two ints
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns mul of  ints
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div: returns div of an int.
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns mod of an int.
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+

43fcd0681094d684e4883a349b087a3f73384b54
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-23 20:58:23 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
- /**
-  * array_iterator - take elems of an array as parameters
-  * @array: pointer to array
-  * @size: size of the array
-  * @action: function pointer
-  * Return: void
-  */
+/**
+ * array_iterator - take elems of an array as parameters
+ * @array: pointer to array
+ * @size: size of the array
+ * @action: function pointer
+ * Return: void
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {

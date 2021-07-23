92f987f19341a22df3a50330a778646ba5d56d2f
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-23 20:36:46 +0300

    Create 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_strings - print strings
+ *@separator: string arg
+ *@n:no
+ *
+ *Return:void
+ */
+unsigned int i;
+char *str;
+va_list ap;
+va_start(ap, n);
+for (i = 0; i < n; i++)
+{
+str = va_arg(ap, char *);
+if (!str)
+str = "(nil)";
+if (!separator)
+printf("%s", str);
+else if (separator ==0 && i ==0)
+printf("%s%s", separator, str);
+}
+printf("\n");
+va_end(ap);
+}

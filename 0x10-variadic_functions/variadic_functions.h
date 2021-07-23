eda110b944331114701b134ae4caabbb43fadcfc
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-07-23 17:37:41 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,16 +1,5 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
-/**
-* struct_op - structure
-*@ch: type
-*@f: function
-*/
-typedef struct op
-{
-  char *ch;
-  void (*f)(va-list arg);
-} op_t;
 int add_them_all(const unsigned int n, ...);
 int _putchar(char *c);
 #endif

a60b7118ad5526f0f747e810c0af485df24b93e6
Bkaraba <bkaraba14@gmail.com>
2021-07-28 09:29:11 -0700

    patch

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,29 +5,37 @@
 
 /**
  * main - sum of arguments
- * @argc: no of argument.
+ * @argc: no of argument
  * @argv:arguments vector.
  *
- *Return: sum of arguments.
+ *Return: on success 0,1 otherwise
  */
 
 int main(int argc, char **argv)
 {
-int result = 0;
-if (argc <= 1)
-{
-printf("0\n");
-}
-else if (isalpha(*(*argv++)++))
-{
-printf("Error\n");
-return (1);
-break;
-}
-else
-{
-result += atoi(*argv++);
-}
-return (0);
+	int i, j, k, num;
+	int result = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		for (k = 1; k < argc; k++)
+		{
+			num = atoi(argv[k]);
+				if (num >= 0)
+				{
+					result += num;
+				}
+		}
+	}
+	printf("%d\n", result);
+	return (0);
 }
 

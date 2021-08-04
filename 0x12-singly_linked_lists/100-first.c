66218396b23eb011deb153949ca8f6579b9d922e
Bkaraba <bkaraba14@gmail.com>
2021-08-03 22:37:24 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * first - prints before main execution
+ *
+ * Return:void
+ */
+void first(void)
+{
+	printf("you're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
+

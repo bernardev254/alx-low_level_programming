7185f35b50ae2cbd2e192b4286b1f99b6f0eb88c
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-07 16:45:44 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,12 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		decimal = 2 * decimal + (b[i] - '0');
+	}
+	for (i = 0; b[i]; i++)
+	{
+		decimal << = 1;
+		if (b[i] == '1')
+			decimal += 1;
 	}
 	return (decimal);
 }

fa387c1bb01d98323115d259539b4612582c8ab7
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-07 17:33:36 +0300

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -26,13 +26,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		*head = last;
 		return (last);
-	}
-	
+	}	
 	while (temp->next)
 		temp = temp->next;
 
 	temp->next = last;
-	
 	return (last);
 }
 

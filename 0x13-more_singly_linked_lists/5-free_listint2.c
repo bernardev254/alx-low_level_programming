2bb7a6bc8e6ccdab50daee4a66cd6df0d110e559
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-07 18:48:27 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		ptr = *head->next;
+		ptr = (*head)->next;
 		free(*head);
 		*head = ptr;
 	}

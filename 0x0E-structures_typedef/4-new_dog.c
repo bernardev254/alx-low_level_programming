52436dfb264b3f296bfb582d3105ce332ed4f865
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-08 12:08:22 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,13 +18,14 @@ if (d == NULL)
 free(d);
 d->name = malloc(sizeof(char) * (strlen(name) + 1));
 if (!d->name)
-free(d->name);
+free(d);
 d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 if (!d->owner)
+{
+free(d);
+free(d->name);
+}
 free(d->owner);
-d->name = name;
-d->age = age;
-d->owner = owner;
 strcpy(d->name, name);
 strcpy(d->owner, owner);
 return (d);

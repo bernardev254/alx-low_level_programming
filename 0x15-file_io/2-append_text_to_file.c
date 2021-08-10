eecf8817cfdb407aaac67fd3cee1012b48105b3c
Bkaraba <bkaraba14@gmail.com>
2021-08-10 07:29:52 -0700

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,17 +6,17 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * append_text_to_file - appends a text to a file
  * @filename: name of the file
  * @text_content: text to write
  *
  * Return: 1 on success,0, otherwise
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w, s = 0;
 
-	fd = open(filename, O_CREATE | O_RDONLY | O_TRUNCATE, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - copies a file to another
+ * @argc: no of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success,98,99,100 otherwise
+ */
+int main(int argc, char **argv)
+{
+	int fd_r, fd_w, rd, c_r, c_w;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: file_from file_to");
+		exit(97);
+	}
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd_w = open(argv[2], O_CREAT | O_RDONLY | O_TRUNC, 0664);
+	while ((rd = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		if (fd_w == -1 || write(fd_w, buf, rd) != rd)
+		{
+			dprintf(STDERR_FILENO, "Error: cant't write to file %s\n", argv[2]);
+			close(fd_r);
+			exit(99);
+		}
+	}
+	if (rd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	c_r = close(fd_r);
+	c_w = close(fd_w);
+	if (c_r < 0 || c_w < 0)
+	{
+		if (c_r < 0)
+			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_r);
+		if (c_w < 0)
+			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_w);
+		exit(100);
+	}
+	return (0);
+}
+
+

b7bc4e800e281a27af2ff8275366c2d3e89b028c
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-13 13:44:55 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 	fd_r = open(argv[1], O_RDONLY);
-	if (fd_r == -1)
+	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -27,14 +27,14 @@ int main(int argc, char **argv)
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((rd = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-		if (fd_w == -1 || write(fd_w, buf, rd) != rd)
+		if (fd_w < 0 || write(fd_w, buf, rd) != rd)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_r);
 			exit(99);
 		}
 	}
-	if (rd == -1)
+	if (rd < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);

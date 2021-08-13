9d9bd72e36af701f477a6847510d25a1aaf91739
Bkaraba <60809345+Bkaraba@users.noreply.github.com>
2021-08-13 13:34:50 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_w = open(argv[2], O_CREAT | O_RDONLY | O_TRUNC, 0664);
@@ -29,14 +29,14 @@ int main(int argc, char **argv)
 	{
 		if (fd_w == -1 || write(fd_w, buf, rd) != rd)
 		{
-			dprintf(STDERR_FILENO, "Error: can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			close(fd_r);
 			exit(99);
 		}
 	}
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	c_r = close(fd_r);
@@ -44,9 +44,9 @@ int main(int argc, char **argv)
 	if (c_r < 0 || c_w < 0)
 	{
 		if (c_r < 0)
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_r);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (c_w < 0)
-			dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd_w);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
 	return (0);

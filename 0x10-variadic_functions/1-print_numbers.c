46ff6c02c0e769f98527f32f7bccde3d76357e43
Bkaraba <bkaraba14@gmail.com>
2021-09-01 08:54:02 -0700

    new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
+
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
@@ -21,5 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s%d", separator, va_arg(list, int));
 	}
 	va_end(list);
+
+	printf("\n");
 }
 

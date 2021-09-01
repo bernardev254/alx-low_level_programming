855f6ec8d8ee634557a6a56f3cc131fe77ba36d4
Bkaraba <bkaraba14@gmail.com>
2021-09-01 04:07:06 -0700

    updates

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,7 +2,7 @@
 #define _VARIADIC_FUNCTIONS_H
 #include <stdio.h>
 #include <stdarg.h>
-int add_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
 int _putchar(char *c);
 void print_strings(const char *separator, const unsigned int n, ...);
 #endif

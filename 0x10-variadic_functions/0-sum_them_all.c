e6bef3502bc7441a297a62e3af01c0ff294cd648
Bkaraba <bkaraba14@gmail.com>
2021-09-01 04:04:45 -0700

    updates

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	
+
 	if (n == 0)
 		return (0);
 	sum = 0;
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,7 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdarg.h>
 int add_them_all(const unsigned int n, ...);
 int _putchar(char *c);
 void print_strings(const char *separator, const unsigned int n, ...);

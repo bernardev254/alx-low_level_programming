7f86a6f7fe22b1b2d703d36970e1ed17574ef6ef
Bkaraba <bkaraba14@gmail.com>
2021-09-09 08:17:03 -0700

    patches

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,11 +12,11 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
+	if (grid == NULL || height == 0)
+		return;
+
 	for (i = 0; i < height; i++)
-	{
-		if (grid[i] != NULL)
-			free(grid[i]);
-	}
+		free(grid[i]);
 
 	free(grid);
 }

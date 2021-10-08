a19383a0693a840f83ab68ad0616b195018eed74
Bkaraba <bkaraba14@gmail.com>
2021-10-08 00:35:30 -0700

    patch

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,8 +21,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new->size = size;
-	new->array = calloc((size_t)new->size, sizeof(hash_node_t));
-	if (!new->array)
+	new->array = calloc((size_t)new->size, sizeof(hash_node_t *));
+	if (new->array == NULL)
 	{
 		return (NULL);
 	}

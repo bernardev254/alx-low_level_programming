321c3d15f7adc88953de41f44b5f46cd6f330b61
Bkaraba <bkaraba14@gmail.com>
2022-06-08 00:44:34 -0700

    fix indexError

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,17 +14,17 @@ def recur(a, b, matrix, perimeter):
     for (ax, bx) in steps:
         new_a = ax + a
         new_b = bx + b
+
         if outside_grid(new_a, new_b, matrix):
             continue
         neibour = matrix[new_a][new_b]
         if neibour == 1:
             perimeter -= 1
-
     return perimeter
 
 
 def outside_grid(a, b, matrix):
-    if a < 0 or b < 0 or a > len(matrix) - 1 or b > len(matrix) - 1:
+    if a < 0 or b < 0 or a > len(matrix) - 1 or b > len(matrix[0]) - 1:
         return True
     return False
 
@@ -36,10 +36,10 @@ def island_perimeter(grid):
     perimeter = 0
     for x, row in enumerate(grid):
         for y, cell in enumerate(row):
-            cell_perim = 0            
+            cell_perim = 0
             if cell == 1:
                 cell_perim = 4
                 cell_perim = recur(x, y, grid, cell_perim)
-            perimeter += cell_perim                
+            perimeter += cell_perim
 
     return perimeter
